479a6ce9ad6524e5da7556b48934016d0eedb4f3
crispy <crispywalrus@gmail.com>
2019-05-07 13:04:40 -0700

    actualize the common modules
    + add an opam file to define spaces_common
    + add a tag cloud module
    + remove the custom env build flags
    + add spaces_common to grapher build

---
+++ lib/tag.ml
@@ -0,0 +1,31 @@
+open Core
+   
+type t = { id : int; tag : string; count : int }
+
+let tag_cloud = Hashtbl.create(module String)
+
+let from_string name =
+  match Hashtbl.find tag_cloud name with
+  | Some tag -> tag
+  | None ->
+     let idx = Hashtbl.fold tag_cloud ~init:0 ~f:(fun ~key:_ ~data:data idx -> if data.id > idx then data.id else idx) in
+     let tag = { id = (idx+1); tag = name; count = 1; } in
+     let _ = Hashtbl.add tag_cloud ~key:name ~data:tag in
+     tag
+
+let to_string tag = tag.tag
+
+let compare a b = Int.compare a.id b.id
+
+let equal a b = phys_equal (compare a b) 0
+
+let greater_than a b = (compare a b) > 0
+                                         
+let greater_than_or_equal a b = (compare a b) >= 0
+
+let less_than a b = (compare a b) < 0
+                                         
+let less_than_or_equal a b = (compare a b) <= 0
+
+                                                 
+                                         
---
+++ lib/tag.mli
@@ -0,0 +1,10 @@
+type t [@@derving show]
+val from_string : string -> t
+val to_string : t -> string
+val compare : t -> t -> int
+val equal : t -> t -> bool
+val greater_than : t -> t -> bool
+val greater_than_or_equal : t -> t -> bool
+val less_than : t -> t -> bool
+val less_than_or_equal : t -> t -> bool
+                                   
