031266e2844e3831c1943faeec390b1043c032ea
chris vale <crispywalrus@gmail.com>
2019-05-13 08:52:19 -0700

    add notes (#10)
    also do a little cleanup to make the api a bit more reliant on the
    janestreet code patterns.

--- lib/weight.ml
+++ lib/weight.ml
@@ -1,3 +1,5 @@
+open Core
+
 type t = Empty | Weight of float [@@deriving show, yojson]
 
 let of_float f = Weight f
@@ -5,3 +7,14 @@ let of_float f = Weight f
 let to_float w = match w with Empty -> None | Weight w -> Some w
 
 let empty = Empty
+
+let compare a b =
+  match (a, b) with
+  | (Empty, Empty) -> 0
+  | (Weight wa, Weight wb) -> Float.compare wa wb
+  | (Empty, Weight _) -> -1
+  | (Weight _, Empty) -> 1
+
+let equal a b =
+  Int.equal (compare a b) 0
+         
