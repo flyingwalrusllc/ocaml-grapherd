235ba16c43a768cf6cda1489dd13a2f9ad261315
chris vale <crispywalrus@gmail.com>
2019-06-05 12:06:04 -0700

    fix growable_array.max not tracking changes (#21)

--- lib/growable_array.ml
+++ lib/growable_array.ml
@@ -2,15 +2,28 @@ open Core
 
 type 'a t = {mutable arr: 'a array; zero: 'a; mutable max: int}
 
+let create size zero =
+  { arr= Array.create ~len:size zero
+  ; zero= zero
+  ; max= 0 }
+  
 let of_array ar zero = {arr= Array.copy ar; zero; max= 0}
 
 let get da i = da.arr.(i)
 
-let set da i v = da.arr.(i) <- v
+let set da i v =
+  let _ = da.arr.(i) <- v in
+  let _ = if da.max < i then
+            da.max <- i
+  in
+  ()
+  
 
 let max da = da.max
 
-let grow g id =
+let capacity ga = Array.length ga.arr
+                
+let ensure g id =
   let rec calc_growth current at_least =
     if current > at_least then current else calc_growth (current * 2) at_least
   in
@@ -18,9 +31,8 @@ let grow g id =
   let growth = n_size - Array.length g.arr in
   if growth > 0 then
     let n_arr = Array.create ~len:growth g.zero in
-    let _ = g.arr <- Array.append g.arr n_arr in
-    g
-  else g
+    g.arr <- Array.append g.arr n_arr
+  else ()
 
 let%test_module _ =
   ( module struct
@@ -33,11 +45,11 @@ let%test_module _ =
     let%test "set existing" =
       let _ = set start 50 50 in
       let x = get start 50 in
-      Int.equal x 50
+      (Int.equal x 50) && Int.equal 50 @@ max start
 
     let%test "grow" =
-      let _ = grow start 1000 in
+      let _ = ensure start 1000 in
       let _ = set start 666 23 in
       let x = get start 666 in
-      Int.equal x 23
+      (Int.equal x 23) && Int.equal 666 @@ max start
   end )
--- lib/growable_array_intf.ml
+++ lib/growable_array_intf.ml
@@ -1,13 +1,20 @@
+include Base
+      
 module type Growable_array = sig
   type 'a t
-
+        
+  val create : int -> 'a -> 'a t
+     
   val of_array : 'a array -> 'a -> 'a t
-
+  
   val get : 'a t -> int -> 'a
-
+  
   val set : 'a t -> int -> 'a -> unit
-
+  
   val max : 'a t -> int
+  
+  val capacity : 'a t -> int
 
-  val grow : 'a t -> int -> 'a t
+  val ensure : 'a t -> int -> unit
+    
 end
